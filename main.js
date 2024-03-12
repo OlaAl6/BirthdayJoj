5d1caa6ea7f2451b547f6df84d4a380fb918d0a3
OlaAl6 <163090115+OlaAl6@users.noreply.github.com>
2024-03-13 02:45:51 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    $(function () {
+        var flame = $("#flame");
+        var txt = $("h1");
+     
+        flame.on({
+            click: function () {
+                flame.removeClass("burn").addClass("puff");
+                $(".smoke").each(function () {
+                    $(this).addClass("puff-bubble");
+                });
+                $("#glow").remove();
+                txt.hide().html("I'm so glad I know you and you inspire me everyday,I hope your year is filled with laughs.Happy Birthday ^3^!!").delay(750).fadeIn(300);
+                $("#candle").animate(
+                    {
+                        opacity: ".5"
+                    },
+                    100
+                );
+            }
+        });
+    });
+});
